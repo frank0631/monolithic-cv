361af1e1e895d47b17df9e83abb8d5e591bd04d6
frank <frank@augmate.com>
2014-11-23 17:26:50 -0500

    Added support PCL

--- testbench/src/main/java/com/frank0631/monolithic/Testbench.java
+++ testbench/src/main/java/com/frank0631/monolithic/Testbench.java
@@ -2,7 +2,8 @@ package com.frank0631.monolithic;
 
 import org.opencv.core.Mat;
 import org.opencv.highgui.Highgui;
-
+import remon_pcl.pcl.*;
+import remon_pcl.pcl.search.KdTree_PointXYZ;
 
 /**
  * Created by frank on 10/18/14.
@@ -11,7 +12,6 @@ public class Testbench {
 
     public static void main(String[] args){
         System.out.println("Hello,OpenCV");
-
         nu.pattern.OpenCV.loadShared();
         System.loadLibrary("opencv_java249");
 
@@ -32,9 +32,13 @@ public class Testbench {
         Mat Book1 = Highgui.imread("resources/books/book1.jpg");
         Mat Book2 = Highgui.imread("resources/books/book2.jpg");
         Mat Book3 = Highgui.imread("resources/books/book3.jpg");
-        ImagePair3DProcess.Quick3DPairProcess(Book2, Book1, showEpipole, showMatch, showStitch, displayWindow,"book2v1","results/book");//Broken 1 is blurry
-        ImagePair3DProcess.Quick3DPairProcess(Book3, Book2, showEpipole, showMatch, showStitch, displayWindow,"book3v2","results/book");
-        ImagePair3DProcess.Quick3DPairProcess(Book3, Book1, showEpipole, showMatch, showStitch, displayWindow,"book3v1","results/book");//Broken 1 is blurry
+        //ImagePair3DProcess.Quick3DPairProcess(Book2, Book1, showEpipole, showMatch, showStitch, displayWindow,"book2v1","results/book");//Broken 1 is blurry
+        //ImagePair3DProcess.Quick3DPairProcess(Book3, Book2, showEpipole, showMatch, showStitch, displayWindow,"book3v2","results/book");
+        //ImagePair3DProcess.Quick3DPairProcess(Book3, Book1, showEpipole, showMatch, showStitch, displayWindow,"book3v1","results/book");//Broken 1 is blurry
+
+        //Point Cloud Wrapper test
+        System.loadLibrary("PCL_Wrapper");
+        program1_test();
 
         /*//Quick 3D Image Pair processing for joe the bmw
         File joeFolder = new File("training_subset/bmwbayside/4jgda7db8da163624/img");
@@ -56,6 +60,33 @@ public class Testbench {
             System.out.println("fundamental " + inputParams.pairNames + "\n" + fundHomoMat.fundamentaAB.dump());
             System.out.println();
         }*/
+    }
+
+    static void program1_test()
+    {
+        PointCloud_PointXYZ cloud =new PointCloud_PointXYZ();
+        //passing points
+        cloud.push_back(new PointXYZ((float)0.025, (float)0.060, (float)0.070));
+        cloud.push_back(new PointXYZ((float)0.026, (float)0.060, (float)0.070));
+        cloud.push_back(new PointXYZ((float)0.027, (float)0.060, (float)0.070));
+        cloud.push_back(new PointXYZ((float)0.028, (float)0.060, (float)0.070));
+        // Create the normal estimation class, and pass the input dataset to it
+        NormalEstimation_PointXYZ_Normal ne=new NormalEstimation_PointXYZ_Normal();
+        ne.setInputCloud(cloud);
+        KdTree_PointXYZ tree=new KdTree_PointXYZ(0, true);
+        ne.setSearchMethod(tree);
+        PointCloud_Normal cloud_normals=new PointCloud_Normal();
+        ne.setRadiusSearch(0.03);
+        ne.compute(cloud_normals);
+        System.out.println("Normal Cloud Size:"+cloud_normals.size());
+        for (int i = 0; i < cloud_normals.size(); i++) {
+            Normal N=cloud_normals.at(i);
+            System.out.print("Point ");
+            System.out.println(i);
+            System.out.println(N.getNormal_x());
+            System.out.println(N.getNormal_y());
+            System.out.println(N.getNormal_z());
+        }
     }
 
 }
